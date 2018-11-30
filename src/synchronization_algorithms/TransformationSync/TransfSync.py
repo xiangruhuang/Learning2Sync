6a72b4299d4e9347fa6ef203c3f3aa1439b814fe
xiangruhuang <xrhuang@cs.utexas.edu>
2018-11-29 22:38:14 -0600

    TransfSync

--- src/synchronization_algorithms/TransformationSync/TransfSync.py
+++ src/synchronization_algorithms/TransformationSync/TransfSync.py
@@ -5,29 +5,9 @@ import glob
 from scipy.linalg import block_diag
 import sys
 sys.path.append('../../../')
-from util import angular_distance_np
+from util import angular_distance_np, inverse, pack, decompose
 import scipy.io as sio
 import pathlib
-
-def __decompose__(T):
-    R = T[:3, :3]
-    t = T[:3, 3]
-    return R, t
-
-def __pack__(R, t):
-    T = np.zeros((4, 4))
-    T[:3, :3] = R
-    T[:3, 3] = t
-    T[3, 3] = 1.0
-    return T
-
-def inverse(T):
-    R, t = __decompose__(T)
-    invT = np.zeros((4, 4))
-    invT[:3, :3] = R.T
-    invT[:3, 3] = -R.T.dot(t)
-    invT[3, 3] = 1
-    return invT
 
 """
     Find a matrix Q \in O(n) such that \|A Q - B\|_F is minimized
@@ -64,11 +44,11 @@ def generate_synthetic(n, sigma):
             if i <= j:
                 continue
             Tij = T[j].dot(inverse(T[i]))
-            Rij, tij = __decompose__(Tij)
+            Rij, tij = decompose(Tij)
             Rij = Rij + np.random.randn(3, 3) * sigma
             Rij = project_so(Rij)
             tij = tij + np.random.randn(3) * sigma
-            Tij = __pack__(Rij, tij)
+            Tij = pack(Rij, tij)
             edge = {'src':i,
                     'tgt':j,
                     'R': Rij,
@@ -93,7 +73,7 @@ def __normalized_adjacency__(n, edges):
         i = edge['src']
         j = edge['tgt']
         Rij = edge['R']
-        weight = edge['weight']
+        weight = edge['translation_weight']
         A[(i*3):(i+1)*3, (j*3):(j+1)*3] = weight*Rij.T
         A[(j*3):(j+1)*3, (i*3):(i+1)*3] = weight*Rij
         deg[i] += weight
@@ -119,7 +99,7 @@ def laplacian(n, edges):
         i = edge['src']
         j = edge['tgt']
         Rij = edge['R']
-        weight = edge['weight']
+        weight = edge['rotation_weight']
         A[(i*3):(i+1)*3, (j*3):(j+1)*3] = -weight*Rij.T
         A[(j*3):(j+1)*3, (i*3):(i+1)*3] = -weight*Rij
         deg[i] += weight
@@ -128,30 +108,19 @@ def laplacian(n, edges):
     for i in range(n):
         A[i*3:(i+1)*3, i*3:(i+1)*3] = np.eye(3) * deg[i]
 
-    #Dinv = np.kron(np.diag(1.0/deg), np.eye(3))
-
-    #Anorm = sqrtm(Dinv).dot(A).dot(sqrtm(Dinv))
     return A, deg
 
 def from_laplacian(L):
     n = L.shape[0]//3
-    #deg = np.diag(L)[::3]
-    #Dinv = np.kron(np.diag(1.0/deg), np.eye(3))
     
     lamb, V = np.linalg.eigh(L)
 
-    #print(lamb)
-    #lamb = abs(lamb)
-    #idx = np.argsort(lamb)
-    #print(lamb[idx])
     eigengap = lamb[-3] - lamb[-4]
     for i in range(n):
         if V[0, i] < 0.0:
             V[:, i] = -V[:, i]
     if np.linalg.det(V[:3, :3]) < 0:
         V[:, :] = -V[:, :]
-    #dsqrt = np.sqrt(deg.sum())
-    #V = sqrtm(Dinv).dot(V) * dsqrt
     
     A = []
     R = []
@@ -171,54 +140,9 @@ def from_laplacian(L):
 """
 def Spectral(n, edges):
     L, deg = laplacian(n, edges)
-    #Lnorm = np.eye(n*3)-Anorm
     R, eigengap, A, lamb, V = from_laplacian(L)
     return R, eigengap
-    #Dinv = np.kron(np.diag(1.0/deg), np.eye(3))
 
-    #lamb, V = np.linalg.eigh(L)
-    ##print(lamb)
-    ##lamb = abs(lamb)
-    ##idx = np.argsort(lamb)
-    ##print(lamb[idx])
-    #eigengap = lamb[-3] - lamb[-4]
-    #V = V[:, :3]
-    #dsqrt = np.sqrt(deg.sum())
-    ##V = sqrtm(Dinv).dot(V) * dsqrt
-
-    #R = []
-    #for i in range(n):
-    #    Ri = V[i*3:(i+1)*3, :]
-    #    Ri = project_so(Ri)
-    #    R.append(Ri)
-
-    #R = np.array(R)
-
-    #return R, eigengap
-
-#"""
-#    Solve min_{ti, tj} \sum_{i, j} wij \| Rj tij + tj - ti \|^2
-#    Gradient Descent (only for debug)
-#"""
-#def __LeastSquaresGD__(n, edges, R):
-#    t = np.zeros((n, 3))
-#    lr = 1e-5
-#    for itr in range(10000):
-#        grad = np.zeros((n, 3))
-#        loss = 0.0
-#        for edge in edges:
-#            i = edge['i']
-#            j = edge['j']
-#            Rij = edge['Tij'][:3, :3]
-#            tij = edge['Tij'][:3, 3]
-#            weight = edge['weight']
-#            loss += weight * np.linalg.norm(Rij.dot(t[i]) + tij - t[j], 2) ** 2
-#            grad[i, :] += 2.0*weight*(t[i] + Rij.T.dot(tij) - Rij.T.dot(t[j]))
-#            grad[j, :] += 2.0*weight*(t[j] - Rij.T.dot(t[i]) - tij)
-#        t = t - grad * lr
-#        if itr % 100 == 0:
-#            print('iter=%d, loss=%f' % (itr, loss))
-#    return t
 
 """
     Solve min_{ti, tj} \sum_{i, j} wij \| Rj tij + tj - ti \|^2
@@ -240,7 +164,7 @@ def LeastSquares(n, edges):
         j = edge['tgt']
         Rij = edge['R']
         tij = edge['t']
-        weight = edge['weight']
+        weight = edge['translation_weight']
         
         #L[i*3:(i+1)*3, j*3:(j+1)*3] -= weight * Rij.T
         #L[j*3:(j+1)*3, i*3:(i+1)*3] -= weight * Rij
@@ -250,37 +174,27 @@ def LeastSquares(n, edges):
         b[j*3:(j+1)*3] += weight*tij
 
     t = np.linalg.lstsq(L, b)[0]
-    #print(L.shape, t.shape)
-    #print('Loss=%f' % np.linalg.norm(L.dot(t) - b, 2))
     return t
-
-#def find(x, f):
-#    if f[x] == f[f[x]]:
-        
-
-#def connected(n, edges):
-#    f = np.zeros(n)
-#    for i in range(n):
-#        f[i] = i
-#    for edge in edges:
-#        i = edge['src']
-#        j = edge['tgt']
-#        
 
 def error(T, G):
     aerrs = []
     n = T.shape[0]
+    terrs = []
     for i in range(n):
         for j in range(i+1, n):
             Ti = T[i, :, :]; Tj = T[j, :, :]
             Gi = G[i, :, :]; Gj = G[j, :, :]
-            Rij = Tj.dot(inverse(Ti))[:3, :3]
-            Rij_gt = Gj.dot(inverse(Gi))[:3, :3]
+            Tij = Tj.dot(inverse(Ti))
+            Gij = Gj.dot(inverse(Gi))
+            Rij = Tij[:3, :3]
+            Rij_gt = Gij[:3, :3]
             fro = np.linalg.norm(Rij - Rij_gt, 'fro')
             aerr = angular_distance_np(Rij[np.newaxis, :, :], Rij_gt[np.newaxis, :, :]).sum()
-            #print(fro, aerr)
+            terr = np.linalg.norm(Tij[:3, 3]-Gij[:3, 3], 2)
             aerrs.append(aerr)
-    return np.mean(aerrs)
+            terrs.append(terr)
+
+    return np.mean(aerrs), np.mean(terrs)
 
 def coin(p):
     if np.random.uniform() < p:
@@ -299,7 +213,7 @@ def max_existing_err(n, edges, R, t):
         i = edge['src']
         j = edge['tgt']
         Rij = edge['R']
-        weight = edge['weight']
+        weight = edge['rotation_weight']
         if weight < 1e-10:
             continue
         err = np.linalg.norm(R[j].dot(R[i].T) - Rij, 'fro')
@@ -308,34 +222,46 @@ def max_existing_err(n, edges, R, t):
         
     return max_err 
 
-def truncatedWeightPredict(n, edges, R, t, eps0):
+#def truncatedWeightPredict(n, edges, R, t, eps0):
+#    for edge in edges:
+#        i = edge['src']
+#        j = edge['tgt']
+#        Rij = edge['R']
+#        weight = edge['rotation_weight']
+#        #if edge['predicted_weight'] < 0.5:
+#        #    edge['weight'] = 0.
+#        #    continue
+#        if weight < 1e-6:
+#            continue
+#        err = np.linalg.norm(R[j].dot(R[i].T) - Rij, 'fro')
+#        if err > eps0 - 1e-12:
+#            edge['weight'] = 0.0
+
+def reweightEdges(n, edges, R, t, sigma_r, sigma_t):
+    theta1 = 2
+    terrs = []
     for edge in edges:
         i = edge['src']
         j = edge['tgt']
         Rij = edge['R']
-        weight = edge['weight']
-        #if edge['predicted_weight'] < 0.5:
-        #    edge['weight'] = 0.
-        #    continue
-        if weight < 1e-6:
-            continue
-        err = np.linalg.norm(R[j].dot(R[i].T) - Rij, 'fro')
-        if err > eps0 - 1e-12:
-            edge['weight'] = 0.0
-def reweightEdges(n, edges, R, t, sigma=0.01):
-    theta1 = 8
-    for edge in edges:
-        i = edge['src']
-        j = edge['tgt']
-        Rij = edge['R']
+        tij = edge['t']
+
+        Tij_in = pack(Rij, tij)
+        Ti = pack(R[i], t[i])
+        Tj = pack(R[j], t[j])
+        Tij_rec = Tj.dot(inverse(Ti))
+
         weight = edge['predicted_weight']
-        if weight < 0.001:
-            weight = 0.001
+        if weight < 0.01:
+            weight = 0.00
         if weight > 1.0:
             weight = 1.0
-        fro2 = np.linalg.norm(R[j].dot(R[i].T) - Rij, 'fro')
-        edge['weight'] = (sigma ** theta1) / (sigma ** theta1 + fro2 ** theta1) * (weight ** theta1)
-        #print(edge['weight'], edge['predicted_weight'])
+        
+        aerr_fro = np.linalg.norm(Tij_rec[:3, :3] - Tij_in[:3, :3], 'fro')
+        terr_fro = np.linalg.norm(Tij_rec[:3, 3] - Tij_in[:3, 3], 2)
+        #fro2 = aerr_fro ** 2 + terr_fro ** 2
+        edge['rotation_weight'] = (sigma_r ** theta1) / (sigma_r ** theta1 + aerr_fro ** (theta1) ) * weight
+        edge['translation_weight'] = (sigma_t ** theta1) / (sigma_t ** theta1 + terr_fro ** (theta1) ) * weight
 
 def computeStats(n, edges, R, t):
     cover = np.zeros(n, dtype=np.int32)
@@ -345,7 +271,7 @@ def computeStats(n, edges, R, t):
         i = edge['src']
         j = edge['tgt']
         Rij = edge['R']
-        weight = edge['weight']
+        weight = edge['rotation_weight']
         if weight < 1e-10:
             continue
         err = np.linalg.norm(R[j].dot(R[i].T) - Rij, 'fro') ** 2
@@ -356,36 +282,36 @@ def computeStats(n, edges, R, t):
         numedges += 1
     return min(cover), numedges, err_sum
 
-def dump_edges(n, edges, Tstar, path):
-    L = np.zeros((n*3, n*3))
-    label = np.zeros((n, n))
-    deg = np.zeros(n)
-    good = 0
-    bad = 0
-    for edge in edges:
-        src = edge['src']
-        tgt = edge['tgt']
-        Rij = edge['R']
-        Ti = Tstar[src]; Tj = Tstar[tgt]
-        Tij_gt = Tj.dot(inverse(Ti))
-        aerr = angular_distance_np(Rij[np.newaxis, :, :], Tij_gt[np.newaxis, :3, :3]).sum()
-        labelij = edge['weight']
-        if labelij > 0.5:
-            if aerr < 30.0:
-                good += 1
-            else:
-                bad += 1
-        label[src, tgt] = labelij
-        label[tgt, src] = labelij
-        L[src*3:(src+1)*3, tgt*3:(tgt+1)*3] = -Rij.T
-        deg[src] += 1.0
-        deg[tgt] += 1.0
-    L = L + L.T
-    for i in range(n):
-        L[i*3:(i+1)*3, i*3:(i+1)*3] = deg[i]*np.eye(3)
-    print('good=%d, bad=%d' % (good, bad))
-    pathlib.Path('./predict_new/%s' % path).mkdir(exist_ok=True, parents=True)
-    sio.savemat('./predict_new/%s.mat' % path, mdict={'Laplacian': L, 'label': label, 'Rstar': Tstar[:, :3, :3]})
+#def dump_edges(n, edges, Tstar, path):
+#    L = np.zeros((n*3, n*3))
+#    label = np.zeros((n, n))
+#    deg = np.zeros(n)
+#    good = 0
+#    bad = 0
+#    for edge in edges:
+#        src = edge['src']
+#        tgt = edge['tgt']
+#        Rij = edge['R']
+#        Ti = Tstar[src]; Tj = Tstar[tgt]
+#        Tij_gt = Tj.dot(inverse(Ti))
+#        aerr = angular_distance_np(Rij[np.newaxis, :, :], Tij_gt[np.newaxis, :3, :3]).sum()
+#        labelij = edge['weight']
+#        if labelij > 0.5:
+#            if aerr < 30.0:
+#                good += 1
+#            else:
+#                bad += 1
+#        label[src, tgt] = labelij
+#        label[tgt, src] = labelij
+#        L[src*3:(src+1)*3, tgt*3:(tgt+1)*3] = -Rij.T
+#        deg[src] += 1.0
+#        deg[tgt] += 1.0
+#    L = L + L.T
+#    for i in range(n):
+#        L[i*3:(i+1)*3, i*3:(i+1)*3] = deg[i]*np.eye(3)
+#    print('good=%d, bad=%d' % (good, bad))
+#    pathlib.Path('./predict_new/%s' % path).mkdir(exist_ok=True, parents=True)
+#    sio.savemat('./predict_new/%s.mat' % path, mdict={'Laplacian': L, 'label': label, 'Rstar': Tstar[:, :3, :3]})
     
 
 def fakeTruncatedTransfSync(n, edges, eps0=-1, decay=0.99, Tstar=None, max_iter=10000, cheat=False, pc = None, idx = None, path=None): 
@@ -404,7 +330,7 @@ def edge_quality(n, edges, Tstar, R):
         Ti = Tstar[sid]; Tj = Tstar[tid]
         Tij_gt = Tj.dot(inverse(Ti))
         Rij_in = edge['R']; tij_in = edge['t']
-        Tij_in = __pack__(Rij_in, tij_in)
+        Tij_in = pack(Rij_in, tij_in)
         aerr_gt = angular_distance_np(Tij_in[np.newaxis, :3, :3], Tij_gt[np.newaxis, :3, :3]).sum()
         Rij = R[tid].dot(R[sid].T)
         aerr = np.linalg.norm(Rij - Rij_in, 'fro') ** 2
@@ -417,7 +343,14 @@ def edge_quality(n, edges, Tstar, R):
     print('Edge Quality: #good=%f, #bad=%f, mean aerr=(%f, %f)' % (good_edges, bad_edges, err_good / good_edges, err_bad / bad_edges))
     
 
-def TruncatedTransfSync(n, edges, eps0=-1, decay=0.8, Tstar=None, max_iter=10000, cheat=False):
+def IterativeTransfSync(n, edges, eps0=-1, decay=0.8, Tstar=None, 
+    max_iter=10000, cheat=False, scheme='reweight'):
+    
+    if scheme == 'reweight':
+        reweight = True
+    else:
+        reweight = False
+
     if cheat:
         for edge in edges:
             #if edge['weight'] < 0.5:
@@ -426,14 +359,17 @@ def TruncatedTransfSync(n, edges, eps0=-1, decay=0.8, Tstar=None, max_iter=10000
             Ti = Tstar[sid]; Tj = Tstar[tid]
             Tij_gt = Tj.dot(inverse(Ti))
             Rij = edge['R']; tij = edge['t']
-            Tij = __pack__(Rij, tij)
+            Tij = pack(Rij, tij)
             aerr = angular_distance_np(Tij[np.newaxis, :3, :3], Tij_gt[np.newaxis, :3, :3]).sum()
+            terr = np.linalg.norm(Tij[:3, 3] - Tij_gt[:3, 3], 2)
             #p = 0.9
-            if aerr > 30.0:
-                edge['weight'] = coin(0.03)
+            if aerr > 30.0 or terr > 0.2:
+                weight = coin(0.03)
             else:
-                edge['weight'] = coin(0.8)
-            edge['predicted_weight'] = edge['weight']
+                weight = coin(0.9)
+            edge['predicted_weight'] = weight
+            edge['translation_weight'] = weight
+            edge['rotation_weight'] = weight
 
     """ Edge Quality """
     good_edges = 0.0
@@ -441,46 +377,42 @@ def TruncatedTransfSync(n, edges, eps0=-1, decay=0.8, Tstar=None, max_iter=10000
     err_bad = 0.0
     err_good = 0.0
     for edge in edges:
-        if edge['weight'] < 0.01:
+        if edge['predicted_weight'] < 0.01:
             continue
         sid = edge['src']; tid = edge['tgt']
         Ti = Tstar[sid]; Tj = Tstar[tid]
         Tij_gt = Tj.dot(inverse(Ti))
         Rij = edge['R']; tij = edge['t']
-        Tij = __pack__(Rij, tij)
+        Tij = pack(Rij, tij)
         aerr = angular_distance_np(Tij[np.newaxis, :3, :3], Tij_gt[np.newaxis, :3, :3]).sum()
         if aerr > 30.0:
             #print(sid, tid, edge['predicted_weight'])
             bad_edges += 1
-            err_bad += aerr * edge['weight']
+            err_bad += aerr * edge['predicted_weight']
         else:
             good_edges += 1
-            err_good += aerr * edge['weight']
+            err_good += aerr * edge['predicted_weight']
     print('Edge Quality: #good=%f, #bad=%f' % (good_edges, bad_edges))
-    reweight = False
     itr = 0
     while itr < max_iter:
-        try:
-            R, t, eigengap = TransfSync(n, edges)
-        except Exception as e:
-            print(e)
-            break
+        R, t, eigengap = TransfSync(n, edges)
         #edge_quality(n, edges, Tstar, R)
-        T = np.array([__pack__(R[i], t[i]) for i in range(n)])
+        T = np.array([pack(R[i], t[i]) for i in range(n)])
         err_gt = -1.0
         if Tstar is not None:
-            err_gt = error(T, Tstar)
+            aerr_gt, terr_gt = error(T, Tstar)
         
-        if eps0 < -0.5:
-            eps0 = max_existing_err(n, edges, R, t)
+        if not reweight:
+            if eps0 < -0.5:
+                eps0 = max_existing_err(n, edges, R, t)
 
         if reweight:
-            reweightEdges(n, edges, R, t, sigma=0.1)
+            reweightEdges(n, edges, R, t, sigma_r=0.05, sigma_t=0.01)
         else:
             truncatedWeightPredict(n, edges, R, t, eps0)
 
         mindeg, numedges, err_sum = computeStats(n, edges, R, t)
-        print('iter=%d, avg(err^2)=%f, eigengap=%f, #edges=%d, min_deg=%f, eps0=%f, aerr_gt=%f' % (itr, err_sum/numedges, eigengap, numedges, mindeg, eps0, err_gt))
+        print('iter=%d, avg(err^2)=%f, eigengap=%f, #edges=%d, min_deg=%f, eps0=%f, aerr_gt=%f, terr_gt=%f' % (itr, err_sum/numedges, eigengap, numedges, mindeg, eps0, aerr_gt, terr_gt))
     
         """ Skip idle iterations """
         if reweight:
--- src/synchronization_algorithms/TransformationSync/main.py
+++ src/synchronization_algorithms/TransformationSync/main.py
@@ -2,6 +2,8 @@ import sys
 sys.path.append('../../../')
 import argparse
 import scipy.io as sio
+from TransfSync import IterativeTransfSync
+import numpy as np
 
 parser = argparse.ArgumentParser(
     description='Perform Transformation Synchronization')
@@ -35,7 +37,27 @@ if args.truncate:
 if args.input is None:
     raise ValueError('input file needs to be specified')
 
-mat = sio.loadmat(args.input)['T']
+mat = sio.loadmat(args.input)
 
-print(mat.keys())
+T = mat['T']
+Tstar = mat['Tstar']
+n = Tstar.shape[0]
+edges = []
+for i in range(n):
+    for j in range(i+1, n):
+        edge = {}
+        edge['src'] = i
+        edge['tgt'] = j
+        Tij = T[i*4:(i+1)*4, j*4:(j+1)*4]
+        assert abs(Tij[3, 3] - 1.0) < 1e-3
+        R = Tij[:3, :3]
+        assert np.linalg.norm(R.dot(R.T) - np.eye(3), 'fro') < 1e-3
+        assert np.linalg.det(R) > 0.01
+        edge['R'] = R
+        edge['t'] = Tij[:3, 3]
+        edge['rotation_weight'] = 1.0
+        edge['translation_weight'] = 1.0
+        edge['predicted_weight'] = 1.0
+        edges.append(edge)
 
+Tsync = IterativeTransfSync(n, edges, Tstar = Tstar, cheat=True, scheme=args.scheme)
